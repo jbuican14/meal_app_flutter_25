dbe3b4a28030bea712062158abfc9050fc7bc2ca
Juti <jbuican19@gmail.com>
2025-04-13 19:16:38 +0100

    build meal screen
    when meals is not empty then render
    also use theme for styles

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,11 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    Widget content = Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'No meals in this category',
+            style: Theme.of(context)
+                .textTheme
+                .headlineLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Please try select a different category',
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.onSurface),
+          )
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: meals.length,
+        itemBuilder: (ctx, index) => Text(meals[index].title),
+      );
+    }
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        // using Listview for optimisation where list/text is long
+        body: content);
   }
 }
