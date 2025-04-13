f7452011f36ec6a12528d39926b479a8973b912d
Juti <jbuican19@gmail.com>
2025-04-13 19:17:37 +0100

    Update categories.dart

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_meal_app/data/dummy_data.dart';
+import 'package:flutter_meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -7,30 +9,20 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Pick your category'),
+          title: const Text('Pick your category !'),
         ),
         body: GridView(
+          padding: const EdgeInsets.all(24),
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             childAspectRatio: 3 / 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20,
           ),
-          children: const [
-            Text(
-              '1',
-              style: TextStyle(color: Color.fromARGB(255, 185, 230, 207)),
-            ),
-            Text('2',
-                style: TextStyle(color: Color.fromARGB(255, 185, 230, 207))),
-            Text('3',
-                style: TextStyle(color: Color.fromARGB(255, 185, 230, 207))),
-            Text('4',
-                style: TextStyle(color: Color.fromARGB(255, 185, 230, 207))),
-            Text('5',
-                style: TextStyle(color: Color.fromARGB(255, 185, 230, 207))),
-            Text('6',
-                style: TextStyle(color: Color.fromARGB(255, 185, 230, 207))),
+          children: [
+            // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+            for (final category in availableCategories)
+              CategoryGridItem(category: category)
           ],
         ));
   }
