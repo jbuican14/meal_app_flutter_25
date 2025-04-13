3dda0a581e60228834ed70ecfd7cb9b268006648
Juti <jbuican19@gmail.com>
2025-04-13 19:15:30 +0100

    test meals screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_meal_app/data/dummy_data.dart';
+import 'package:flutter_meal_app/screens/meals.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_meal_app/categories.dart';
+// import 'package:flutter_meal_app/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -23,7 +24,12 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      // home: const CategoriesScreen(),
+      // test meal screen
+      home: const MealsScreen(
+        title: "Some dummy category...",
+        meals: dummyMeals,
+      ),
     );
   }
 }
