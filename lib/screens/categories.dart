71e86e0dd76d9b8465a5453a3571e1e1c49f54a4
Juti <jbuican19@gmail.com>
2025-04-13 19:50:44 +0100

    Navigator.push method
    using Navigator built in flutter method to push screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_meal_app/data/dummy_data.dart';
+import 'package:flutter_meal_app/screens/meals.dart';
 import 'package:flutter_meal_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  //build switch screen
+  void _selectCategory(BuildContext context) {
+    // built in flutter to change screen Navigaor.of(context).push(route)
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: 'title', meals: []),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +34,12 @@ class CategoriesScreen extends StatelessWidget {
           children: [
             // availableCategories.map((category) => CategoryGridItem(category: category)).toList()
             for (final category in availableCategories)
-              CategoryGridItem(category: category)
+              CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectCategory(context);
+                },
+              )
           ],
         ));
   }
